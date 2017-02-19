2a9cfbae56568d99e90d5d052d74cbb36cf79768
Alexander Jones <happy5214@gmail.com>
2017-02-19 17:14:46 -0600

    Initial implementation of soccer leagues
    Much of the code was taken from my earlier FILG code.
    
    Tests and full docstrings will come later.

---
+++ competitions/league/DoubleRoundRobinLeague.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8  -*-
+"""Double round-robin league."""
+
+# Copyright (C) 2014-17 Alexander Jones
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import print_function, unicode_literals
+
+from . import FixtureLeague, GoalBasedTeamSeason, WinLoseDrawTeamSeason
+from competitions.scheduler.roundrobin import DoubleRoundRobinScheduler
+
+
+class DoubleRoundRobinLeague(FixtureLeague):
+
+    """
+    A double round-robin competition, usually a domestic league.
+
+    This class does not recognize multi-level or divisional league structures.
+    """
+
+    def __init__(self, match_class, teams):
+        """Constructor.
+
+        @param match_class: The class of the match simulator.
+        @type match_class: A Match-like object.
+        @param teams: The teams competing in this league.
+        @type teams: list of Team-like objects.
+        """
+        super(DoubleRoundRobinLeague, self).__init__(match_class=match_class,
+                                                     teams=teams)
+        self._schedule = DoubleRoundRobinScheduler(self._teams).generate_schedule()
+
+    def _wrap_teams(self):
+        """Wrap teams in TeamSeason objects."""
+        self._teams = []
+        for team in self._raw_teams:
+            self._teams.append(self.TeamSeason(team))
+
+    def play_round(self):
+        """Simulates a round of matches."""
+        Match = self._match_class
+        round_matches = self._schedule[self.round]
+        for teams in round_matches:
+            match = Match(teams[0], teams[1])
+            match.play()
+            self._merge_stats(match)
+            self._fixtures[teams] = match
+        self._round += 1
+        self._teams.sort(key=self.TeamSeason.key, reverse=True)
+
+    def print_standings(self):
+        """Prints the league table to the console."""
+        print("After Round {0}:".format(self.round))
+        print(self.standings_header)
+        for team in self._teams:
+            print(team.standings_str())
+
+    def print_fixtures(self):
+        """Prints the fixture table to the console."""
+        for home in self._teams:
+            for away in self._teams:
+                if home == away or ((home, away) not in self._fixtures):
+                    print("{:<7} ".format(" "))
+                    continue
+                print("{:<7} ".format(self._fixtures[(home, away)].score_str()))
+            print
+
+
+class AssociationFootballDoubleRoundRobinLeague(DoubleRoundRobinLeague):
+
+    """A typical association football (football or soccer) double round-robin league."""
+
+    standings_header = "{:<30}\t{:>5}\t{:>5}\t{:>5}\t{:>5}\t{:>5}\t{:>5}\t{:>5}".format(
+        "Team", "W", "L", "D", "Pts", "GF", "GA", "GD"
+    )
+
+    class TeamSeason(WinLoseDrawTeamSeason, GoalBasedTeamSeason):
+        @staticmethod
+        def key(x):
+            return (x.points, x.goal_diff, x.goals_for)
+
+        @property
+        def points(self):
+            return self.wins * 3 + self.draws
+
+        def standings_str(self):
+            return "{:<30}\t{:>5}\t{:>5}\t{:>5}\t{:>5}\t{:>5}\t{:>5}\t{:>5}".format(
+                self, self.wins, self.losses, self.draws, self.points,
+                self.goals_for, self.goals_against, self.goal_diff
+            )
+
+    def _merge_stats(self, match):
+        team1 = match.team1
+        team2 = match.team2
+        if match.drawn:
+            team1.drew()
+            team2.drew()
+        else:
+            match.winner.won()
+            match.loser.lost()
+        team1.scored(match.score1)
+        team2.allowed(match.score1)
+        team2.scored(match.score2)
+        team1.allowed(match.score2)
--- competitions/league/__init__.py
+++ competitions/league/__init__.py
@@ -17,3 +17,165 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import unicode_literals
+
+from competitions.match import Match
+
+
+class League(object):
+
+    """A generic league-style competition."""
+
+    def __init__(self, match_class=Match, teams=[]):
+        """Constructor."""
+        self._raw_teams = teams
+        self._wrap_teams()
+        self._round = 0
+        self._match_class = match_class
+
+    @property
+    def round(self):
+        """The current round."""
+        return self._round
+
+    def _wrap_teams(self):
+        """Wrap team objects in TeamSeason objects."""
+        raise NotImplementedError
+
+    def _merge_stats(self, match):
+        """Merge match stats into the TeamSeason objects."""
+        raise NotImplementedError
+
+    def play_round(self):
+        """Simulates a round of matches."""
+        raise NotImplementedError
+
+    def play_season(self):
+        try:
+            while True:
+                self.play_round()
+        except IndexError:
+            return self._teams[0]
+
+    def print_standings(self):
+        """Prints the league table/standings to the console."""
+        raise NotImplementedError
+
+
+class FixtureLeague(League):
+
+    """A league with printable fixtures."""
+
+    def __init__(self, match_class, teams):
+        """Constructor.
+
+        @param match_class: The class of the match simulator.
+        @type match_class: A Match-like object.
+        @param teams: The teams competing in this league.
+        @type teams: list of Team-like objects.
+        """
+        super(FixtureLeague, self).__init__(match_class=match_class,
+                                            teams=teams)
+        self._fixtures = {}
+
+    def print_fixtures(self):
+        """Prints the fixture table to the console."""
+        raise NotImplementedError
+
+
+class TeamSeason(object):
+
+    """A team's participation in a league."""
+
+    def __init__(self, team):
+        self._team = team
+
+    def __unicode__(self):
+        """Return a unicode representation of the team's season.
+
+        This simply returns the unicode version of the team.
+        @return: The team's season as a unicode string
+        @rtype: unicode
+        """
+        return unicode(self._team)
+
+    def __str__(self):
+        """Return a str representation of the team's season.
+
+        The format of the string is the same as that returned by __unicode__,
+        except that it is a str object instead of a unicode object.
+        @return: The team's season as a str
+        @rtype: str
+        """
+        return str(self.__unicode__())
+
+    @property
+    def team(self):
+        return self._team
+
+    @property
+    def key(self):
+        raise NotImplementedError
+
+    @property
+    def standings_str(self):
+        raise NotImplementedError
+
+
+class WinLoseDrawTeamSeason(TeamSeason):
+
+    """A TeamSeason based on wins, losses, and draws."""
+
+    def __init__(self, team):
+        super(WinLoseDrawTeamSeason, self).__init__(team)
+        self._wins = 0
+        self._losses = 0
+        self._draws = 0
+
+    @property
+    def wins(self):
+        return self._wins
+
+    @property
+    def losses(self):
+        return self._losses
+
+    @property
+    def draws(self):
+        return self._draws
+
+    def won(self):
+            self._wins += 1
+
+    def lost(self):
+        self._losses += 1
+
+    def drew(self):
+        self._draws += 1
+
+
+class GoalBasedTeamSeason(TeamSeason):
+
+    """A TeamSeason based on goals (use PointBasedTeamSeason if points are used)."""
+
+    def __init__(self, team):
+        super(GoalBasedTeamSeason, self).__init__(team)
+        self._goals_for = 0
+        self._goals_against = 0
+
+    @property
+    def goals_for(self):
+        return self._goals_for
+
+    @property
+    def goals_against(self):
+        return self._goals_against
+
+    @property
+    def goal_diff(self):
+        return self._goals_for - self._goals_against
+
+    def scored(self, goals):
+        self._goals_for += goals
+
+    def allowed(self, goals):
+        self._goals_against += goals
